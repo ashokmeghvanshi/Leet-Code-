dc744bbadd3c9360b0bcfd8842278b665c2f797c
ASHOK KUMAR MEGHVANSHI <42780456+ashokmeghvanshi@users.noreply.github.com>
2021-02-21 23:59:27 +0530

    Easy Level Problem

---
+++ LeetCode/Easy/Valid Boomerang.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isBoomerang(self, points: List[List[int]]) -> bool:
+        
+        x0,y0=points[0][0],points[0][1]
+        x1,y1=points[1][0],points[1][1]
+        x2,y2=points[2][0],points[2][1]
+        
+        a=(y1-y0)*(x2-x1)
+        b=(y2-y1)*(x1-x0)
+        
+        return a!=b
+        
