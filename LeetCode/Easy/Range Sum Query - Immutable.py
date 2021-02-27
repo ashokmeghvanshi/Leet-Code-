14fa40b55a6bb64677f0c401e4d74deebca3e147
ASHOK KUMAR MEGHVANSHI <42780456+ashokmeghvanshi@users.noreply.github.com>
2021-02-27 23:47:22 +0530

    Easy Level Problem

---
+++ LeetCode/Easy/Range Sum Query - Immutable.py
@@ -0,0 +1,24 @@
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.nums=nums
+        if len(self.nums)==0:
+            return None
+        
+        self.dp=[0]*(len(self.nums))
+        s=0
+        j=0
+        for i in self.nums:
+            s=s+i
+            self.dp[j]=s
+            j=j+1
+        
+    def sumRange(self, i: int, j: int) -> int:
+        if i==0:
+            return self.dp[j]
+        return self.dp[j]-self.dp[i-1]
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(i,j)
