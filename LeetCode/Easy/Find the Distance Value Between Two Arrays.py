de69eed25a15a23ad10b0e3fc63485642b6f5580
ASHOK KUMAR MEGHVANSHI <42780456+ashokmeghvanshi@users.noreply.github.com>
2021-04-19 23:48:40 +0530

    code update

--- LeetCode/Easy/Find the Distance Value Between Two Arrays.py
+++ LeetCode/Easy/Find the Distance Value Between Two Arrays.py
@@ -2,9 +2,12 @@ class Solution:
     def findTheDistanceValue(self, arr1: List[int], arr2: List[int], d: int) -> int:
         s=0
         for i in arr1:
+            t=0
             for j in arr2:
                 if abs(i-j)<= d:
+                    t=1
                     break
-            else:
+            if t==0:
                 s=s+1
+            #print(s,i)
         return s
